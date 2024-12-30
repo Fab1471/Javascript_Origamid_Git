c540e4f24f61d27ec086bd3a7dc88e703bfcdb38
Fabri <ism.fabri@gmail.com>
2024-12-29 21:19:50 -0300

    finalizandoDia29.12.24

---
+++ 0202/1.script.js
@@ -0,0 +1,30 @@
+var nome = "Fabri";
+console.log(typeof nome);
+var idade = 29;
+console.log(typeof idade);
+var time;
+time = null;
+console.log(typeof time);
+var símbolo = Symbol();
+console.log(typeof símbolo);
+
+var nome = "Fabri";
+var sobrenome = "Figueiredo";
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = "Romário fez " + gols + " gols";
+console.log(frase);
+console.log(typeof frase);
+
+var ano = "2045";
+var mes = 11;
+console.log(ano + mes);
+
+var frase1 = 'Esse é o "melhor" jogo';
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols * 2} gols`;
+console.log(frase2);
---
+++ 0202/script.js
@@ -0,0 +1,20 @@
+var string = "Boné";
+console.log(typeof string);
+
+var number = "123";
+console.log(typeof number);
+
+var idade = 29;
+
+var nome = "Fabri";
+var sobrenome = "Figueiredo";
+
+var frase = "It's time";
+
+console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos.`);
+
+console.log(typeof nome);
+
+var verificarTipoNome = typeof nome;
+
+console.log(verificarTipoNome);
