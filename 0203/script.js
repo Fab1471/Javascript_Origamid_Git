741d11794da7b538e7d732c8f9b757a0852b7f9c
Fabri <ism.fabri@gmail.com>
2024-12-31 02:52:30 -0300

    finalizando31.12.24

---
+++ 0203/script.js
@@ -0,0 +1,28 @@
+var total = 10 + (5 * 2) / 2 + 20;
+console.log(total);
+
+var exp1 = "is10";
+var exp2 = "10is";
+
+console.log(exp1);
+console.log(exp2);
+console.log(isNaN(exp1));
+console.log(isNaN(exp2));
+
+var a = "200";
+var b = 50;
+console.log(+a + b);
+
+var incremento = 5;
+console.log(++incremento);
+
+var numero = "80";
+var unidade = "kg";
+var peso = numero + unidade;
+var pesoPorDois = numero / 2;
+console.log(peso);
+console.log(pesoPorDois);
+
+var pesoBy2 = numero / 2;
+var result = pesoBy2 + unidade;
+console.log(result);
---
+++ 0203/script_aula.js
@@ -0,0 +1,29 @@
+var idade = 29;
+console.log(idade);
+
+var soma = 100 + 50;
+var subtracao = 100 - 50;
+var multiplicacao = 100 * 2;
+var divisao = 100 / 2;
+var expoente = 2 ** 4;
+var modulo = 14 % 5;
+
+var soma = "100" + 50; // 10050
+var subtracao = "100" - 50; // 50
+var multiplicacao = "100" * "2"; // 200
+var divisao = "Comprei 10" / 2; // NaN (Not a Number)
+
+// NaN = Not a Number
+
+// É possível verificar se uma variável é NaN ou não com a função isNaN()
+
+// Ordem de Precedência
+
+var x = 5;
+console.log(++x);
+console.log(x);
+
+// Unários
+// --  e ++  antes da variável = incrementa ou decrementa a variável e retorna o valor
+
+// se botar - ou + na frente da string ele tenta transformar em número
